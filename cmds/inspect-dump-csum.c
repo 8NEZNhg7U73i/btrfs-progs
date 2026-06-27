bfeb77c8458c812f2ad40968cc35921dee0d9783
example <example@example.com>
2026-06-27 15:29:24 +0000

    Update inspect-dump-csum.c

--- cmds/inspect-dump-csum.c
+++ cmds/inspect-dump-csum.c
@@ -124,10 +124,6 @@ static int btrfs_lookup_csums(struct btrfs_trans_handle *trans, struct btrfs_roo
 			// possibly,some other csums on this item.
 			for (i = 0; i < pending_csums; i++, cnt++)
 			{
-				if (cnt % 8 == 0)
-				{
-					pr_default("\n");
-				}
 				read_extent_buffer(leaf, tree_csum, (unsigned long)item + ((i * csum_size) + start_pos), csum_size);
 				print_32_bytes_hex(tree_csum, csum_size);
 			}
@@ -138,10 +134,6 @@ static int btrfs_lookup_csums(struct btrfs_trans_handle *trans, struct btrfs_roo
 		{
 			for (i = 0; i < csums_in_item; i++, cnt++)
 			{
-				if (cnt % 8 == 0)
-				{
-					pr_default("\n");
-				}
 				read_extent_buffer(leaf, tree_csum, (unsigned long)item + ((i * csum_size) + start_pos), csum_size);
 				print_32_bytes_hex(tree_csum, csum_size);
 			}
