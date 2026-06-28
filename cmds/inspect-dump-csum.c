73581c070167a3008bede6fc222fdd1a2c70a811
example <example@example.com>
2026-06-28 04:35:21 +0000

    Update inspect-dump-csum.c

--- cmds/inspect-dump-csum.c
+++ cmds/inspect-dump-csum.c
@@ -51,6 +51,7 @@ void print_32_bytes_hex_le(const uint8_t *bytes, const uint8_t length)
 	pr_default(" ");
 }
 
+void print_32_bytes_hex_be(const uint8_t *bytes, const uint8_t length)
 {
 	for (uint8_t i = 0; i < length; i++)
 	{
